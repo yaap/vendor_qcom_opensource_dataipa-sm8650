949ab07e78324bba5f8804077bf9a02f4b634bc6
Jagadeesh Ponduru <quic_jponduru@quicinc.com>
2023-09-22 10:49:09 +0530

    msm: ipa3: disable monaco compilation temporarily
    Disable IPA compilation for monaco as a temp change.
    
    Change-Id: I872cf2d8bd6a0ae95ae18073c71d336528a11d97
    Signed-off-by: Jagadeesh Ponduru <quic_jponduru@quicinc.com>

--- drivers/platform/msm/Android.mk
+++ drivers/platform/msm/Android.mk
@@ -1,7 +1,7 @@
 ifeq ($(TARGET_DATAIPA_DLKM_ENABLE), true)
 ifneq ($(TARGET_BOARD_PLATFORM),qssi)
 
-GSI_DLKM_PLATFORMS_LIST := taro kalama bengal monaco pineapple blair holi
+GSI_DLKM_PLATFORMS_LIST := taro kalama bengal pineapple blair holi
 
 #Enabling BAZEL
 LOCAL_MODULE_DDK_BUILD := true
